a81f1dcdad9c4740ebe1d892dd7f6cb2dd01844f
valljoa <joan.vallat@divtec.ch>
2023-09-26 15:27:26 +0200

    Test de la saisie et envoie du formulaire

--- js/main.js
+++ js/main.js
@@ -40,11 +40,24 @@ const formulaire = document.querySelector('form');
 const txtPassword = document.querySelector('#pass');
 const txtPasswordConfirm = document.querySelector('#confirmPass');
 
-//Ecouter l'envoie du formulaire
-formulaire.addEventListener('submit',function (){
+//Écouter l'envoie du formulaire
+formulaire.addEventListener('submit',function (event){
+    //Stopper l'envoie ou annuler évènement
+    event.preventDefault();
+
+    //Si mdp plus petit que 3 caractères
+    if (txtPassword.value.length < 3){
+        alert('Les mot de pass doit avoir au minimu 3 caractères');
+        return; //termine la fonction
+    }
+
+    //Si mdp différent
     if (txtPassword.value !== txtPasswordConfirm.value) {
         alert('Mot de pass différents');
-        return;
+        return; //termine la fonction
     }
+
+    //Alerte l'utilisateur
     alert('Compte créer avec succès !')
+    formulaire.submit(); //Envoie le formulaire
 });
